dc42ce6603753d291e1d988f182febff7140b8df
Bryce Jacobs <popasquat89@gmail.com>
2015-05-06 16:53:50 -0500

    test - dumb mistake

--- test/nshm.js
+++ test/nshm.js
@@ -4,18 +4,17 @@ var shmId = nshm.open(0xCBA5FFF0, 'a', 0x1000, 0660);
 
 console.log('Shared Memory Id: %d', shmId);
 
-function readSHM (){
-    console.log('Attempting Read');
-    console.log(nshm.read(shmId));
-}
-
 var attempts = 10;
-while (attempts){
-
-    setTimeout(function (){
-        readSHM();
-        attempts--;
-    }, 500)
+function readSHM(){
+    while (attempts){
+        console.log('Attempting Read');
+        console.log(nshm.read(shmId));
+        setTimeout(function (){
+            readSHM();
+            attempts--;
+        }, 500)
+    }
 }
 
+readSHM();
 
